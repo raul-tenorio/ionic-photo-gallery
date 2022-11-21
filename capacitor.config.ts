99cdb8f0d93a2222dcfd0bd00575a3b3c4cc9430
Raul Tenorio <raul.tenorio@softwarehouse.com.ec>
2022-11-20 19:54:47 -0500

    Proyect Update

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-first-proyect',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
